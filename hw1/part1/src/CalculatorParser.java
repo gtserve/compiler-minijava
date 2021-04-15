427bef39dba6543a31c6e655ed60b14595b80419
George Tservenis <george.tservenis@gmail.com>
2021-04-15 21:52:22 +0300

    Move CalculatorParser

---
+++ hw1/part1/src/CalculatorParser.java
@@ -0,0 +1,17 @@
+import java.io.InputStream;
+import java.io.IOException;
+
+public class CalculatorParser {
+
+    private InputStream in;
+    private int lookahead;
+
+
+    public static void main(String[] args) {
+        try {
+            /* code */
+        } catch (IOException | ParseError exc) {
+            System.err.println(exc.getMessage());
+        }
+    }
+}
