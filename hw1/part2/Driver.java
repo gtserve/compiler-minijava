4d6d9433f562072d962c130d2d8d6a52a38b3462
George Tservenis <george.tservenis@gmail.com>
2021-04-17 16:07:19 +0300

    first implementation done

--- hw1/part2/Driver.java
+++ hw1/part2/Driver.java
@@ -1,7 +1,7 @@
 import java_cup.runtime.*;
 import java.io.*;
 
-class Main {
+class Driver {
     public static void main(String[] argv) throws Exception{
         Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
         p.parse();
